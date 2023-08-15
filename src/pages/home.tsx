0aa2f481c52948d84a5c9786d2871b313f7a089a
Imam Hidayat <imhidayat100602@gmail.com>
2023-08-15 19:36:10 +0700

    Initial commit

---
+++ src/pages/home.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "../components/home";
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <main className="grid">
+        <h1 className="">This is Main</h1>
+      </main>
+    </>
+  );
+}
